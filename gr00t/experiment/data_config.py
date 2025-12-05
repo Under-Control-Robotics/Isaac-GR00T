ef2a8b5d51aa35249ae77410be6c4a57099bb66d
Anthony-huo-ucr <mxhuo@ucr.bot>
2025-12-05 18:23:44 -0500

    align current data config

--- gr00t/experiment/data_config.py
+++ gr00t/experiment/data_config.py
@@ -1276,7 +1276,7 @@ class UCRWBLMMobyHistoryDataConfig(BaseDataConfig):
     # Separate indices for each modality
     video_observation_indices = [-30, 0]
     state_observation_indices = [0]
-    action_indices = list(range(64))
+    action_indices = list(range(16))
 
     def modality_config(self) -> dict[str, ModalityConfig]:
         video_modality = ModalityConfig(
