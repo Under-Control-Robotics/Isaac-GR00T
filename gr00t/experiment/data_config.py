04008da608eb00092e3b09434a2cbca97ea5856c
Sergey Rogulenko <sergey@ucr.bot>
2025-12-10 09:25:12 -0800

    Align robot joints with hardware
    And remove hardcoded cuda visible device

--- gr00t/experiment/data_config.py
+++ gr00t/experiment/data_config.py
@@ -1256,20 +1256,19 @@ class UCRWBLMMobyHistoryDataConfig(BaseDataConfig):
     state_keys = [
         "state.waist_joint",
         "state.right_arm_joint",
-        "state.left_arm_joint",
         "state.right_leg_joint",
+        "state.left_arm_joint",
         "state.left_leg_joint",
-        "state.orientation_joint",
     ]
     action_keys = [
         "action.behavior_mode",
         "action.left_ee_position",
-        "action.right_ee_position",
         "action.left_ee_orientation",
+        "action.right_ee_position",
         "action.right_ee_orientation",
+        "action.base_vel",
         "action.base_height",
         "action.base_orientation",
-        "action.base_vel",
     ]
     language_keys = ["annotation.human.action.task_description"]
 
